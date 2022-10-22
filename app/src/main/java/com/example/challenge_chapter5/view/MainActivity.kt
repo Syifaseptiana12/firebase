2fcf026c5895a126a6f053692b3d82d7d3f75c6f
surezluvy <raynaldizoel@gmail.com>
2022-10-23 05:42:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/challenge_chapter5/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.challenge_chapter5.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.challenge_chapter5.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
